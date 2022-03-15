4f41486cf07c64024075296e93741c7cc3ff5119
Kevin Steves <kevin.steves@pobox.com>
2022-03-15 14:16:13 -0700

    0.0.0.dev1

--- paniot/__init__.py
+++ paniot/__init__.py
@@ -20,7 +20,7 @@ import logging
 import re
 import sys
 
-__version__ = '0.0.0.dev0'
+__version__ = '0.0.0.dev1'
 
 _default_api_version = (4, 0)
 DEFAULT_API_VERSION = 'v%d.%d' % _default_api_version
