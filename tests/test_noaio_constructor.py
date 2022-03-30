a1f3cf2ab6836bca36af1f71672f6217b7e59f50
Kevin Steves <kevin.steves@pobox.com>
2022-03-30 11:46:20 -0700

    Fix to remove mixin_constructor; it didn't work for async tests.

--- tests/test_noaio_constructor.py
+++ tests/test_noaio_constructor.py
@@ -2,10 +2,77 @@ import asyncio
 import unittest
 
 import paniot
-from . import mixin_constructor
 
 
-class IotApiTest(unittest.TestCase,
-                 mixin_constructor.MixinConstructor):
+class IotApiTest(unittest.TestCase):
+    def tearDown(self):
+        if hasattr(self, 'api') and hasattr(self.api, 'session'):
+            self.api.session.close()
+
     def test_00(self):
         self.assertRaises(RuntimeError, asyncio.get_running_loop)
+
+    def test_01(self):
+        kwargs = {}
+        with self.assertRaises(paniot.ArgsError) as e:
+            self.api = paniot.IotApi(**kwargs)
+        self.assertEqual(str(e.exception), 'customerid required')
+
+    def test_02(self):
+        kwargs = {
+            'customerid': 'x',
+        }
+        with self.assertRaises(paniot.ArgsError) as e:
+            self.api = paniot.IotApi(**kwargs)
+        self.assertEqual(str(e.exception), 'access_key_id required')
+
+    def test_03(self):
+        kwargs = {
+            'customerid': 'x',
+            'access_key_id': 'x',
+        }
+        with self.assertRaises(paniot.ArgsError) as e:
+            self.api = paniot.IotApi(**kwargs)
+        self.assertEqual(str(e.exception), 'access_key required')
+
+    def test_04(self):
+        kwargs = {
+            'customerid': 'x',
+            'access_key_id': 'x',
+            'access_key': 'x',
+            'api_version': 'x',
+        }
+        with self.assertRaises(paniot.ArgsError) as e:
+            self.api = paniot.IotApi(**kwargs)
+        self.assertRegex(str(e.exception),
+                         '^Invalid api_version')
+
+    def test_05(self):
+        kwargs = {
+            'customerid': 'x',
+            'access_key_id': 'x',
+            'access_key': 'x',
+        }
+        self.api = paniot.IotApi(**kwargs)
+        with self.assertRaises(paniot.ArgsError) as e:
+            self.api.decode_jwt()
+        self.assertEqual(str(e.exception),
+                         'token is not a JSON Web Signature')
+
+    def test_06(self):
+        kwargs = {
+            'customerid': 'x',
+            'access_key_id': 'x',
+            'access_key': 'x',
+        }
+        self.api = paniot.IotApi(**kwargs)
+        for x in [self.api.device,
+                  self.api.device_details,
+                  self.api.vulnerability,
+                  self.api.alert,
+                  self.api.tag,
+                  self.api.device_update,
+                  self.api.vuln_update,
+                  self.api.alert_update]:
+            self.assertTrue(x.window > 0)
+            self.assertTrue(x.rate_limit > 0)
