be1fd81d75532724e72bd21935a2e3614d52c67d
Kevin Steves <kevin.steves@pobox.com>
2022-04-15 18:35:49 -0700

    Add tag tests for offset and pagelength.

--- tests/test_aio_tag.py
+++ tests/test_aio_tag.py
@@ -13,4 +13,25 @@ class IotApiTest(mixin.AioMixin, unittest.IsolatedAsyncioTestCase):
         resp = await self.api.tag()
         self.assertEqual(resp.status, 200)
         x = await resp.json()
-        self.assertEqual(x['totalTags'], len(x['tags']))
+        if x['totalTags'] <= 1000:
+            self.assertEqual(x['totalTags'], len(x['tags']))
+        if x['totalTags'] > 0:
+            resp = await self.api.tag(pagelength=1)
+            self.assertEqual(resp.status, 200)
+            x = await resp.json()
+            self.assertEqual(len(x['tags']), 1)
+            if x['totalTags'] > 1:
+                tag0 = x['tags'][0]
+                resp = await self.api.tag(offset=0,
+                                          pagelength=2)
+                self.assertEqual(resp.status, 200)
+                x = await resp.json()
+                self.assertEqual(len(x['tags']), 2)
+                self.assertEqual(x['tags'][0], tag0)
+                tag1 = x['tags'][1]
+                resp = await self.api.tag(offset=1,
+                                          pagelength=1)
+                self.assertEqual(resp.status, 200)
+                x = await resp.json()
+                self.assertEqual(len(x['tags']), 1)
+                self.assertEqual(x['tags'][0], tag1)
--- tests/test_noaio_tag.py
+++ tests/test_noaio_tag.py
@@ -13,4 +13,25 @@ class IotApiTest(mixin.Mixin, unittest.TestCase):
         resp = self.api.tag()
         self.assertEqual(resp.status_code, 200)
         x = resp.json()
-        self.assertEqual(x['totalTags'], len(x['tags']))
+        if x['totalTags'] <= 1000:
+            self.assertEqual(x['totalTags'], len(x['tags']))
+        if x['totalTags'] > 0:
+            resp = self.api.tag(pagelength=1)
+            self.assertEqual(resp.status_code, 200)
+            x = resp.json()
+            self.assertEqual(len(x['tags']), 1)
+            if x['totalTags'] > 1:
+                tag0 = x['tags'][0]
+                resp = self.api.tag(offset=0,
+                                    pagelength=2)
+                self.assertEqual(resp.status_code, 200)
+                x = resp.json()
+                self.assertEqual(len(x['tags']), 2)
+                self.assertEqual(x['tags'][0], tag0)
+                tag1 = x['tags'][1]
+                resp = self.api.tag(offset=1,
+                                    pagelength=1)
+                self.assertEqual(resp.status_code, 200)
+                x = resp.json()
+                self.assertEqual(len(x['tags']), 1)
+                self.assertEqual(x['tags'][0], tag1)
